f16585e521f75bdcfe0a05b4b5484f0e03546bf3
Daniel Leite de Oliveira <dleitee@gmail.com>
2019-05-30 13:36:16 -0300

    Add initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const isProduction = process.env.APP_ENV === "production";
+
+module.exports = {
+  entry: {
+    app: [path.resolve(__dirname, "src", "index.js")]
+  },
+  mode: process.env.APP_ENV,
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: isProduction ? "[name].[hash].js" : "[name].js",
+    publicPath: "/static/",
+    hotUpdateChunkFilename: "hot/hot-update.js",
+    hotUpdateMainFilename: "hot/hot-update.json"
+  }
+};
