d1543c6c1596a5ef1b431bfe405b25fd0dbac271
Alisson Granemann Abreu <alissongranemannabreu@gmail.com>
2019-06-13 10:34:11 -0300

    fix eslint errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require("path");
+const path = require('path')
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv')
 
-dotenv.config();
+dotenv.config()
 
-const isProduction = process.env.APP_ENV === "production";
+const isProduction = process.env.APP_ENV === 'production'
 
 module.exports = {
   entry: {
-    app: [path.resolve(__dirname, "src", "index.js")]
+    app: [path.resolve(__dirname, 'src', 'index.js')],
   },
   mode: process.env.APP_ENV,
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: isProduction ? "[name].[hash].js" : "[name].js",
-    publicPath: "/static/",
-    hotUpdateChunkFilename: "hot/hot-update.js",
-    hotUpdateMainFilename: "hot/hot-update.json"
-  }
-};
+    path: path.resolve(__dirname, 'dist'),
+    filename: isProduction ? '[name].[hash].js' : '[name].js',
+    publicPath: '/static/',
+    hotUpdateChunkFilename: 'hot/hot-update.js',
+    hotUpdateMainFilename: 'hot/hot-update.json',
+  },
+}
