9f1fccb8318b913ee1df71f2f5cdab958bd66d52
Mibs Balsante <mibsbalsante@ckl.io>
2020-03-02 11:33:40 -0300

    refactor: sync storybook files with ui toolkit

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const Dotenv = require('dotenv-webpack')
+
+const webpackConfig = require('../webpack.extra')
 
 module.exports = async ({ config }) => {
   const rules = config.module.rules.filter(rule => !rule.test.test('.css'))
@@ -26,7 +29,7 @@ module.exports = async ({ config }) => {
           loader: 'postcss-loader',
           options: {
             config: {
-              path: path.resolve('../', process.cwd()),
+              path: path.resolve(__dirname),
             },
           },
         },
@@ -47,7 +50,24 @@ module.exports = async ({ config }) => {
         },
       ],
     },
+    {
+      test: /\.(eot|svg|ttf|woff|jpg|png)/,
+      use: ['file-loader'],
+    },
   ]
+
+  config.plugins.push(
+    new Dotenv({
+      path: path.join(__dirname, '..', '.env'), // load this now instead of the ones in '.env'
+      safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
+      systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+      silent: true, // hide any errors
+      defaults: false, // load '.env.defaults' as the default values if empty.
+    })
+  )
+
+  // eslint-disable-next-line no-param-reassign
+  config.resolve = webpackConfig.resolve
 
   return config
 }
